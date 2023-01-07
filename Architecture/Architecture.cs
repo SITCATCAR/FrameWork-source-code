a8af43a13189100cb0d3cf074aef65c7139ef2a8
SITCATCAR <117428030+SITCATCAR@users.noreply.github.com>
2023-01-07 13:34:57 +0800

    Update Architecture.cs
    some annotations

--- Architecture/Architecture.cs
+++ Architecture/Architecture.cs
@@ -7,6 +7,7 @@ namespace FrameworkDesign
     public abstract class Architecture<T> where T : Architecture<T>, new()
     {
         private static T mArchiteure;//mArchiteure 本质是一个字典
+        //确保 Container 有实例
         static void MakeSureArchitecture()
         {
             if (mArchiteure == null)
@@ -15,17 +16,17 @@ namespace FrameworkDesign
                 mArchiteure.Init();
             }
         }
-
+        //子类注册模块
         protected abstract void Init();
-
         private IOCContainer mContainer = new IOCContainer();
+        //获取模块的 API
         public static T Get<T>() where T : class
         {
             MakeSureArchitecture();
 
             return mArchiteure.mContainer.Get<T>();
         }
-
+        //提供一个注册模块的 API
         public void Regester<T>(T instance)
         {
             MakeSureArchitecture();
